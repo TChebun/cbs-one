86fd0368ebc5a1d1cd102cfca68d286d04be76e1
Tatiana <t.chebun@gmail.com>
2020-06-29 18:56:07 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,440 @@
+let transactions = [{
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+13.7653 </span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbseouldotio</span>',
+    balance: '<span class="out">-159.1769</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.9273</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-5.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-12.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+6.5128</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-2.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+2.9421</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-5.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+1.1354</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="out">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+5.4634</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+135.1273</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.3542</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-16.6421</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="out">-8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+5.4634</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+135.1273</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.3542</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-16.6421</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+6.5128</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-2.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+2.9421</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-5.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+1.1354</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="out">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+5.4634</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+135.1273</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.3542</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-16.6421</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="out">-8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+5.4634</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+135.1273</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.3542</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-16.6421</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+6.5128</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-2.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+2.9421</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-5.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="inn">+1.1354</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="out">+8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+5.4634</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+135.1273</span> CBSCH'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.3542</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-16.6421</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-3.png" alt="item-logo"> <span>cbsiomeetone</span>',
+    balance: '<span class="out">-8.1213</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+5.4634</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-2.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+135.1273</span> CBS'
+  },
+  {
+    rank: '<img src="img/inn.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-4.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="inn">+6.3542</span> CBS'
+  },
+  {
+    rank: '<img src="img/out.svg" alt="rank">',
+    date: '26 apr 2020 <span>&#8226; 14:16</span>',
+    name: '<img src="img/logo-1.png" alt="item-logo"> <span>cbshuobipool</span>',
+    balance: '<span class="out">-16.6421</span> CBS'
+  }
+]
+
+let account = document.getElementById('account'),
+  hideMenu = document.querySelector('.header__account'),
+  accountCode = document.querySelector('.account__code'),
+  menuLink = document.querySelectorAll('.menu__link'),
+  copyBtn = document.querySelector('.account__name-button'),
+  accountName = document.querySelector('.account__name'),
+  tabs = document.querySelectorAll('.inn-out'),
+  balanceTabs = document.querySelectorAll('.balance-mod'),
+  prevMenuLink = menuLink[0],
+  prevTabs = tabs[0],
+  prevBalanceTab = balanceTabs[0],
+  pagination = document.querySelector('#pagination'),
+  notesOnPage = 15,
+  countOfItems = Math.ceil(transactions.length / notesOnPage),
+  firstPoint = document.getElementById('first');
+
+
+// pagination 
+
+for (let i = 1; i <= countOfItems; i++) {
+  let li = document.createElement('li');
+  li.classList.add('pagination__item');
+  let link = document.createElement('a');
+  pagination.appendChild(li);
+  li.appendChild(link);
+  link.setAttribute('href', '/');
+  link.classList.add('pagination__link');
+  link.innerHTML = i;
+}
+let table = document.querySelector('#tbody');
+
+let items = document.querySelectorAll('#pagination li a');
+
+let showPage = (function () {
+  let pageActive;
+
+  return function (item) {
+    if (pageActive) {
+      pageActive.classList.remove('active');
+    }
+
+    pageActive = item;
+
+    item.classList.add('active');
+
+
+    let pageNum = +item.innerHTML;
+
+    let start = (pageNum - 1) * notesOnPage,
+      end = start + notesOnPage;
+    let notes = transactions.slice(start, end);
+
+    table.innerHTML = '';
+
+    for (let note of notes) {
+      let tr = document.createElement('tr');
+      table.appendChild(tr);
+      tr.classList.add('transaction__body');
+      createCell(note.rank, tr, 'Rank');
+      createCell(note.date, tr, 'Date');
+      createCell(note.name, tr, 'Name');
+      createCell(note.balance, tr, 'Balance');
+    }
+  };
+}());
+
+showPage(items[0]);
+
+for (let item of items) {
+  item.addEventListener('click', function (event) {
+    event.preventDefault();
+    showPage(this);
+  });
+}
+
+function createCell(text, tr, newAttr) {
+  let td = document.createElement('td');
+  td.classList.add('transaction__body-item');
+  td.innerHTML = text;
+  tr.appendChild(td);
+  td.setAttribute('data-label', newAttr);
+}
+
+// account-menu
+
+hideMenu.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (account.classList.contains('active')) {
+    account.classList.remove('active');
+  } else {
+    account.classList.add('active');
+  }
+});
+
+// sidebar
+
+for (let i = 0; i < menuLink.length; i++) {
+  menuLink[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    prevMenuLink.classList.remove('active');
+    this.classList.add('active');
+
+    prevMenuLink = this;
+  })
+}
+
+
+// filter-tabs
+
+let tabContent = document.querySelectorAll('.transaction__body'),
+  tabArray = Array.from(tabContent),
+  innContent;
+
+for (let k = 0; k < tabs.length; k++) {
+
+  tabs[k].addEventListener('click', function () {
+    prevTabs.classList.remove('active');
+    this.classList.add('active');
+
+    prevTabs = this;
+
+    innContent = tabArray.forEach(function (elem) {
+      console.log(elem);
+      
+      
+    });
+
+  });
+}
+
+for (let j = 0; j < balanceTabs.length; j++) {
+  balanceTabs[j].addEventListener('click', function () {
+    prevBalanceTab.classList.remove('active');
+    this.classList.add('active');
+
+    prevBalanceTab = this;
+  });
+}
